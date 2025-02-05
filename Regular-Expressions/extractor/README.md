26ce5d85b65d0f09d277fa05f326bcd2868862c4
tebogomonamodi08 <150713484+tebogomonamodi08@users.noreply.github.com>
2025-02-05 18:59:13 +0200

    This is an extractor application

---
+++ Regular-Expressions/extractor/README.md
@@ -0,0 +1,15 @@
+This program is an extractor it is used to get emails and numbers from a string.
+
+########THE SYSTEM######################
+1. The system can accept string as input.
+2. Find the emails and phone number in the string
+3. Display the result.
+#########################################
+
+######PSEUDO#######################################################
+1. import the re module (to use patterns to find the numbers and email)
+2. import pyperclip module(to make use of the clipboard)
+3. create a match object for the number.
+4. create a match object for the email
+5. loop through the list and display all the emails
+####################################################################
---
+++ Regular-Expressions/extractor/phone-and-email-extractor.py
@@ -0,0 +1,18 @@
+#imports
+import pyperclip
+import re
+
+sample_text = 'Hi my name is Tebogo Monamodi and number is 0652427162, my other number is 0828875523 and my email is tebogomonamodi@gmail.com and my other email is tebogo24@gmail.com'
+
+pyperclip.copy(sample_text) #copy sample text to the clipboard
+number_match_object = re.findall(r'\d{10,12}', pyperclip.paste()) #find the regular expression for numbers
+email_match_object = re.findall(r'[\w\d\.]+@[\w\.]+', pyperclip.paste()) #find the regualr expression for the email
+
+print('The numbers found are: ')
+for i in range(0,len(number_match_object)):
+    print(number_match_object[i])
+print('The emails found are: ')
+for i in range(0, len(email_match_object)):
+    print(email_match_object[i])
+    
+
