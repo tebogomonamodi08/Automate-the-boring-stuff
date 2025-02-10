45781aed7c627576c0e7ffdadcd10ba4f02efb90
tebogomonamodi08 <150713484+tebogomonamodi08@users.noreply.github.com>
2025-02-10 18:57:02 +0200

    Extended functionality of the Multiclip board, delete function added

--- Read-and-Write-Files/sys-module/sys-module-practice.py
+++ Read-and-Write-Files/sys-module/sys-module-practice.py
@@ -8,22 +8,41 @@ try:
     command = sys.argv[1]
 except IndexError:
     sys.exit(0)
-
 if arguments>1:
     if command.lower() == 'save':
-        parameter = sys.argv[2]
+        try:
+            parameter = sys.argv[2]
+        except IndexError:
+            print('The parameter cannot be empty.')
+            sys.exit(1)
         with shelve.open('board') as db:
             try:
                 db[parameter] = pyperclip.paste()
-            except:
+            except pyperclip.PyperclipException:
                 print('Clipboard is empty')
                 sys.exit(1)
     elif command.lower() == 'list':
         with shelve.open('board') as db:
             keys = list(db.keys())
-            pyperclip.copy = keys
+            pyperclip.copy('\n'.join(keys))
+    
+    elif command.lower() == 'delete' and arguments>2:
+        parameter = sys.argv[2]
+        with shelve.open('board') as db:
+            if parameter in db:
+                del db[parameter]
+                print(f'{parameter} deleted from clip.')
+            else:
+                print(f'{parameter} does not exist in the clipboard.')
+        
+
+    elif command.lower() == 'delete' and arguments==2:
+        with shelve.open('board') as db:
+            db.clear()
+            print(f"Shelve cleared")
+
     else:
-        print('Invalid command.')
+        print('Invalid Command')
         sys.exit(1)
     
 else:
